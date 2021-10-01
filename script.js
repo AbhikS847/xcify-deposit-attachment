f54ad62f7270ea1f570e60b4c7f5f64fd0bfe52a
AbhikS847 <abhikshrestha@gmail.com>
2021-10-01 13:29:32 +1000

    Added remove and add picture deposit

--- script.js
+++ script.js
@@ -5,11 +5,21 @@ $(document).ready(function(){
 
         if($ ('[data-name="file"]')[0].files.length === 0){
             $('[data-name="prompt"]').text("No files attached please attach an image file")
-            $('.deposit-attachment-box').find('.btn').removeClass('attached');
+            $('.deposit-attachment-box').removeClass('attached');
+            $('.deposit-attachment-box').find('.btn, h3, [data-name="file"]').show();
         }
         else{
             $('[data-name="prompt"]').text($('[data-name="file"]').val().replace(/^.*\\/, ""));
-            $('.deposit-attachment-box').find('.btn').addClass('attached');
+            $('.deposit-attachment-box').addClass('attached');
+            $('.deposit-attachment-box').find('.btn:not([data-name="btnremove"]), h3, [data-name="file"]').hide();
+            $('.deposit-attachment-box').find('[data-name="btnremove"]').show().on('click',function(){
+                $('[data-name="prompt"]').parent().find('.btn:first-child').remove();
+                $('[data-name="prompt"]').text("No files attached please attach an image file")
+                $('.deposit-attachment-box').find('.btn:not([data-name="btnremove"]), h3, [data-name="file"]').show();
+                $('.deposit-attachment-box').removeClass('attached');
+                $('.deposit-attachment-box').find('[data-name="btnremove"]').hide();
+            });
+            $('.deposit-attachment-box').find('[data-name="prompt"]').parent().prepend('<button class="btn"><i class="fas fa-paperclip"></i></button>');
         }
     })
 });
